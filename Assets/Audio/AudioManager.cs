25b39728423f1a74eda82e17bf5e61538bdf1c2a
kosap89 <126029106+kosap89@users.noreply.github.com>
2025-07-17 03:02:40 +0300

    Added audio and music to the game

---
+++ Assets/Audio/AudioManager.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioMixer _mixer;
+    [SerializeField] AudioClip _music;
+    AudioMixerGroup _musicGroup;
+    AudioMixerGroup _sfxGroup;
+
+    const string MUSIC_GROUP_NAME = "Music";
+    const string SFX_GROUP_NAME = "SFX";
+    const string MASTER_VOLUME_NAME = "MasterVolume";
+    const string MUSIC_VOLUME_NAME = "MusicVolume";
+    const string SFX_VOLUME_NAME = "SFXVolume";
+
+
+    void Init()
+    {
+        _musicGroup = _mixer.FindMatchingGroups(MUSIC_GROUP_NAME)[0];
+        _sfxGroup = _mixer.FindMatchingGroups(SFX_GROUP_NAME)[0];
+
+        PlayAudio(_music, SoundType.Music, 1.0f, true);
+    }
+
+    public void ChangeMasterVolume(float volume)
+    {
+        _mixer.SetFloat(MASTER_VOLUME_NAME, Mathf.Log10(volume) * 20);
+    }
+
+    public void ChangeMusicVolume(float volume)
+    {
+        _mixer.SetFloat(MUSIC_VOLUME_NAME, Mathf.Log10(volume) * 20);
+    }
+
+    public void ChangeSFXVolume(float volume)
+    {
+        _mixer.SetFloat(SFX_VOLUME_NAME, Mathf.Log10(volume) * 20);
+    }
+
+    public static AudioManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+        }
+        Instance = this;
+
+        Init();
+    }
+
+    public enum SoundType
+    {
+        SFX,
+        Music
+    }
+
+    public void PlayAudio(AudioClip audioClip, SoundType soundType, float volume, bool loop)
+    {
+        GameObject newAudioSource = new(audioClip.name + " Source");
+        AudioSource audioSource = newAudioSource.AddComponent<AudioSource>();
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.loop = loop;
+
+        switch (soundType)
+        {
+            case SoundType.SFX:
+                audioSource.outputAudioMixerGroup = Instance._sfxGroup;
+                break;
+            case SoundType.Music:
+                audioSource.outputAudioMixerGroup = Instance._musicGroup;
+                break;
+            default:
+                break;
+        }
+
+        audioSource.Play();
+
+        if (!loop)
+        {
+            Destroy(audioSource.gameObject, audioClip.length);
+        }
+    }
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float _movementSpeed;
     [SerializeField] SpriteRenderer _characterBody;
     [SerializeField] Animator _animator;
+    [SerializeField] AudioClip _footstep;
     Rigidbody2D _rb;
+    float _nextFootstepAudio = 0f;
 
     void Start()
     {
@@ -29,8 +31,27 @@ public class PlayerController : MonoBehaviour
         bool characterIsWalking = movement.magnitude > 0f;
         _animator.SetBool("isWalking", characterIsWalking);
 
+        if (characterIsWalking)
+        {
+            HandleWalkingSounds();
+
+        }
         bool flipSprite = movement.x < 0f;
         _characterBody.flipX = flipSprite;
     }
+    
+    void HandleWalkingSounds()
+        {
+            if (Time.time >= _nextFootstepAudio)
+            {
+                AudioManager.Instance.PlayAudio(_footstep, AudioManager.SoundType.SFX, 1f, false);
+
+                float audioFrequency = _animator.GetCurrentAnimatorClipInfo(0)[0].clip.length / 2f;
+                _nextFootstepAudio = Time.time + audioFrequency;
+            }
+        }
+
+
+        
 }
 
